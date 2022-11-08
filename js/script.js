edc3fb6972db6f7115d5fd0753e124fd1888d816
Letícia Oliveira <leticia8810@gmail.com>
2022-11-08 19:45:23 +0000

    up

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
  /*  TYPING ANIMATION  */
 
  var typed = new Typed(`.typing`, {
-    strings:[``, `Web Developer`, `Web Designer`, `Web and Mobile Student`],
+    strings:[``, `Web Developer`, `Web Designer`, `Dev Student`],
     typeSpeed:100,
     BackSpeed:60,
     loop:true
